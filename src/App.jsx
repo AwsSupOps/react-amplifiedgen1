33a067ad213acc964dfd91313cbc38154e5b9dd2
EC2 Default User <ec2-user@ip-172-31-29-15.ec2.internal>
2025-04-14 14:56:13 +0000

    access env secret3  commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test
         </p>
         <h1>Environment Secret</h1>
-        <p>API Key: {secrets}</p>  
+        <p>API Key: {secrets}</p> 
   
       </div>
       <p className="read-the-docs">
